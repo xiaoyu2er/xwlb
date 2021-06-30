c9a3bf1c72ea764db47c9d3c4a38ebf633c80228
yanqi.zyq <yanqi.zyq@antfin.com>
2021-06-30 15:52:09 +0800

    init

---
+++ index.js
@@ -0,0 +1,223 @@
+const Crawler = require("crawler");
+const fs = require("fs");
+const cheerio = require("cheerio");
+const TurndownService = require("turndown");
+const turndownService = new TurndownService();
+const args = process.argv.slice(2);
+
+const date1 = +new Date(2010, 0, 1);
+const date2 = +new Date(2016, 01, 03);
+
+const today = new Date().setHours(23, 59, 59);
+console.log("Today is", formatDate(today));
+const oneDay = 24 * 60 * 60 * 1000;
+
+const options = {
+  jQuery: {
+    name: "cheerio",
+    options: {
+      decodeEntities: false,
+      normalizeWhitespace: true,
+      xmlMode: true,
+    },
+  },
+  maxConnections: 8,
+};
+function isDate(date) {
+  return Object.prototype.toString.call(date) == "[object Date]";
+}
+
+function parseDate(date) {
+  if (!isDate(date)) {
+    date = new Date(date);
+  }
+  const year = date.getFullYear();
+  const month = ("" + (date.getMonth() + 1)).padStart(2, "0");
+  const day = ("" + date.getDate()).padStart(2, "0");
+
+  return {
+    year,
+    month,
+    day,
+  };
+}
+
+function formatDate(date) {
+  const { year, month, day } = parseDate(date);
+  return `${year}${month}${day}`;
+}
+function getUrl(date) {
+  if (!isDate(date)) {
+    date = new Date(date);
+  }
+  if (+date < date1) {
+    console.log(formatDate(date), "< date1", formatDate(date1));
+    return "";
+  }
+  if (+date > today) {
+    console.log(formatDate(date), "> today", formatDate(today));
+    return "";
+  }
+
+  const str = formatDate(date);
+  
+  if (+date < date2) {
+    // 暂时不解析
+    console.log(formatDate(date), "< date2", formatDate(date2));
+
+    return "";
+    return `http://news.cctv.com/program/xwlb/${str}.shtml`;
+  }
+
+  return `https://tv.cctv.com/lm/xwlb/day/${str}.shtml`;
+}
+
+function getNewsDetailQueues(date) {
+  const urls = [];
+  const url = getUrl(date);
+  if (!url) {
+    return urls;
+  }
+  return new Promise((resolve) => {
+    const c = new Crawler({
+      ...options,
+      callback: (error, res, done) => {
+        const uri = res.request.uri;
+        const href = uri.href;
+        console.log(formatDate(date), href);
+        if (error) {
+          console.error(error);
+          done();
+          return;
+        }
+
+        const $ = res.$;
+
+        $("a").each((i, ele) => {
+          if (i > 0) {
+            urls.push($(ele).attr("href").replace("http://news.cntv.cn/", "http://tv.cctv.com/"));
+          }
+        });
+
+        done();
+      },
+    });
+
+    c.queue(url);
+    c.on("drain", () => {
+      resolve(urls);
+    });
+  });
+}
+
+function getNewsDetail(date, queues) {
+  const result = [];
+  return new Promise((resolve) => {
+    const c = new Crawler({
+      ...options,
+      callback: (error, res, done) => {
+        const uri = res.request.uri;
+        const href = uri.href;
+        if (error) {
+          console.log(formatDate(date), href);
+          console.error(error);
+          done();
+          return;
+        }
+
+        const $ = res.$;
+
+        const title = $(".cnt_nav h3").text().trim().replace("[视频]", "");
+        //   const content = $(".cnt_bd").text();
+        const html = $(".cnt_bd").html();
+        const markdown = turndownService.turndown(html);
+
+        //   console.log(title);
+        //   console.log(html);
+        //   console.log(markdown);
+
+        const index = queues.indexOf(href);
+        result[index] = {
+          href,
+          title,
+          html,
+          markdown,
+        };
+
+        done();
+      },
+    });
+
+    c.queue(queues);
+    c.on("drain", () => {
+      resolve(result);
+    });
+  });
+}
+
+function toFile(date, result, cb) {
+  const main = result
+    .map((r) => {
+      return `## ${r.title}\n\n${r.markdown}\n\n*[原文](${r.href})*\n`;
+    })
+    .join("\n");
+
+  //   const { year, month, day } = parseDate(date);
+  const dateStr = formatDate(date);
+  const content = `# 新闻联播 ${dateStr}\n\n${main}\n`;
+
+  return new Promise((resolve) => {
+    fs.writeFile(`./news/${dateStr}.md`, content, (err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log(`news/${dateStr}.md done`);
+      }
+      resolve(err);
+    });
+  });
+}
+
+async function main(startDate, endDate) {
+  if (!endDate) {
+    endDate = startDate;
+  }
+  days = (+endDate - +startDate) / oneDay;
+
+  const dates = new Array(days + 1).fill(0).map((v, i) => {
+    return new Date(+startDate + i * oneDay);
+  });
+  console.log(
+    "dates",
+    dates.map((d) => {
+      return formatDate(d);
+    })
+  );
+  const ps = dates.map((nowDate) => {
+    const p = (async (date) => {
+      const queues = await getNewsDetailQueues(date);
+
+      console.log(queues);
+      if (!queues.length) {
+        console.log(`${formatDate(nowDate)} 暂无新闻联播`);
+        return;
+      }
+
+      const result = await getNewsDetail(date, queues);
+      await toFile(date, result);
+    })(nowDate);
+
+    return p;
+  });
+
+  await Promise.all(ps);
+  console.log("done");
+
+  //   return;
+}
+
+if (args.length) {
+  const date1 = new Date(args[0]);
+  const date2 = args[1] ? new Date(args[1]) : date1;
+  main(date1, date2);
+}
